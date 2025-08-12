e5c9b3b32f668ba443c23b920e345499a1d72a16
CatastrophicallyCynical <valkyrie.jane.rose@gmail.com>
2025-08-12 15:21:10 -0400

    Update sw.js
    bump cache so all the updates + new theme.js load
    
    Signed-off-by: CatastrophicallyCynical <valkyrie.jane.rose@gmail.com>

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE='gb-cache-v3';
+const CACHE='gb-cache-v4';
 const ASSETS=[
   './',
   './index.html',
   './style.css',
   './app.js',
   './db.js',
+  './theme.js',
   './firebase.js',
   './appMeta.json',
   './affirmations.json',
@@ -16,15 +17,8 @@ const ASSETS=[
   './about.html',
   './recurring.html'
 ];
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
+self.addEventListener('install', (e)=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
 self.addEventListener('activate', (e)=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))) .then(()=>self.clients.claim()));
 });
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
+self.addEventListener('fetch', (e)=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
